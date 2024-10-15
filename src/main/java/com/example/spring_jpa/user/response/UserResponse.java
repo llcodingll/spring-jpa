1bd8ee2c90c7e1f7d0c32dca04a9c8564ed23d9d
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-15 14:44:47 +0900

    create userResponse

--- src/main/java/com/example/spring_jpa/user/response/UserResponse.java
+++ src/main/java/com/example/spring_jpa/user/response/UserResponse.java
@@ -3,13 +3,11 @@ package com.example.spring_jpa.user.response;
 import com.example.spring_jpa.user.domain.User;
 
 public record UserResponse(
+        Long id,
         String email,
         String username
 ) {
-    public User fromEntity() {
-        return User.builder()
-                .email(email)
-                .username(username)
-                .build();
+    public static UserResponse from(User user) { //생성되기 전에 쓰는 건 static 사용
+        return new UserResponse(user.getId(), user.getEmail(), user.getUsername());
     }
 }
