3e2ce2d974eaf4e7806fc3e6dc02b15669660dc2
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-15 10:04:44 +0900

    [init]#2 make user entity

--- src/main/java/com/example/spring_jpa/user/domain/User.java
+++ src/main/java/com/example/spring_jpa/user/domain/User.java
@@ -1,6 +1,9 @@
 package com.example.spring_jpa.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(
@@ -10,17 +13,22 @@ import jakarta.persistence.*;
                 @Index(columnList = "username")
         }
 )
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @Column(name = "USER_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(length = 30, nullable = false)
     private String username;
 
 }
