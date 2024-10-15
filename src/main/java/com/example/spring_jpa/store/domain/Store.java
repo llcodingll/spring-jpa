8b4e6402ce5f009128bb98c53ee7122c2c30ed66
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-15 11:19:13 +0900

    [init]#7 make order entity

--- src/main/java/com/example/spring_jpa/store/domain/Store.java
+++ src/main/java/com/example/spring_jpa/store/domain/Store.java
@@ -1,11 +1,14 @@
 package com.example.spring_jpa.store.domain;
 
+import com.example.spring_jpa.order.domain.Order;
 import com.example.spring_jpa.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "STORES")
@@ -25,4 +28,7 @@ public class Store {
     @ManyToOne
     @JoinColumn(name = "USER_ID")
     private User user;
+
+    @OneToMany(mappedBy = "STORE_ID")
+    private List<Order> orders;
 }
