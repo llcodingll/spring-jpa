97acfdc0e5ffa6fefc85653fb10d970a4fff87ca
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-15 09:47:09 +0900

    table indexing

--- src/main/java/com/example/spring_jpa/user/domain/User.java
+++ src/main/java/com/example/spring_jpa/user/domain/User.java
@@ -1,4 +1,26 @@
 package com.example.spring_jpa.user.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "USERS",
+        indexes = {
+                @Index(columnList = "email"),
+                @Index(columnList = "username")
+        }
+)
 public class User {
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String username;
+
 }
