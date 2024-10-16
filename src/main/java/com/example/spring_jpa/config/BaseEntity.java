8a845a34c0db10c2f76a2af2bbcb1487b99e3cf6
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-16 14:30:30 +0900

    add Last modify date

--- src/main/java/com/example/spring_jpa/config/BaseEntity.java
+++ src/main/java/com/example/spring_jpa/config/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -14,4 +15,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
